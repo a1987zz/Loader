716344bcd5ec3cc4a633e4ca89db4cfc2984e831
a1987zz <a1987zz@mail.ru>
2021-05-24 06:18:15 +0300

    Update GH Injector.h

--- Nuklear_d3d9/Nuklear_d3d9_lite/GH Injector.h
+++ Nuklear_d3d9/Nuklear_d3d9_lite/GH Injector.h
@@ -7,20 +7,20 @@
 #define INJ_UNLINK_FROM_PEB				0x0004
 #define INJ_SHIFT_MODULE				0x0008
 #define INJ_CLEAN_DATA_DIR				0x0010
-#define INJ_THREAD_CREATE_CLOAKED		0x0020
-#define INJ_SCRAMBLE_DLL_NAME			0x0040
+#define INJ_THREAD_CREATE_CLOAKED		        0x0020
+#define INJ_SCRAMBLE_DLL_NAME			        0x0040
 #define INJ_LOAD_DLL_COPY				0x0080
 #define INJ_HIJACK_HANDLE				0x0100
 
 #define INJ_MAX_FLAGS 0x01FF
 
-struct INJECTIONDATA // typedef ïðîïóñêàåòñÿ, êîãäà íå îäíà ïåðåìåííàÿ íå îáúÿâëåííà
+struct INJECTIONDATA
 {
 	DWORD			LastErrorCode;
 	char			szDllPath[MAX_PATH * 2];
 	DWORD			ProcessID;
-	DWORD          	Mode;
-	DWORD	        Method;
+	DWORD              	Mode;
+	DWORD	                Method;
 	DWORD			Flags;
 	//DWORD			hHandleValue;
 	//HINSTANCE		hDllOut;
